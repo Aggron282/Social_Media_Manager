7421148523f9d8a963c0faa5a78de6a01b4347b1
Aggron282 <marcokhodr1@gmail.com>
2025-04-12 01:02:11 -0700

    'l'

--- backend/routes/social_routes.js
+++ backend/routes/social_routes.js
@@ -1,10 +1,9 @@
 const router = require("express").Router();
 const socialController = require("../controllers/social.js");
 
-router.get('/auth/linkedin/userId=:id', socialController.LinkedinStart);
-router.get('/auth/facebook/userId=:id', socialController.MetaStart);
-router.get('/auth/instagram/userId=:id', socialController.MetaStart);
-
+router.get('/auth/linkedin/userId/:id', socialController.LinkedinStart);
+router.get('/auth/facebook/userId/:id', socialController.MetaStart);
+router.get('/auth/instagram/userId/:id', socialController.MetaStart);
 
 router.get('/auth/linkedin/callback', socialController.LinkedinCallback);
 
